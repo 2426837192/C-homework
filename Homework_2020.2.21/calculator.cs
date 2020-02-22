71f71d477469a0e25c5be2d1053075a4170cdabd
Xixi <59766251+2426837192@users.noreply.github.com>
2020-02-22 10:32:55 +0800

    Add files via upload

---
+++ Homework_2020.2.21/calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Int1 = 0, Int2 = 0;
+            double result = 0;
+
+            try
+            {
+                Console.WriteLine("Input the first Integer:");
+                Int1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input the second Integer:");
+                Int2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input a operator:");
+                string Operator = Console.ReadLine();
+
+                switch (Operator)
+                {
+                    case "+":
+                        result = Int1 + Int2;
+                        break;
+                    case "-":
+                        result = Int1 - Int2;
+                        break;
+                    case "*":
+                        result = Int1 * Int2;
+                        break;
+                    case "/":
+                        result = Int1 / Int2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator!");
+                        return;
+                }
+                Console.WriteLine($"The result is:{result}");
+            }
+            
+            catch(OverflowException)
+            {
+                Console.WriteLine("Overflow input!");
+                return;
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Invalid Input!");
+                return;
+            }
+        }
+    }
+}
---
+++ Homework_2020.2.21/calculator_framework.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str1 = num1Box.Text.Trim();
+            string str2 = num2Box.Text.Trim();
+            try
+            {
+                int num1 = Convert.ToInt32(str1);
+                int num2 = Convert.ToInt32(str2);
+
+                switch (comboBox1.Text)
+                {
+                    case "+":
+                        resultBox.Text = (num1 + num2).ToString();
+                        break;
+                    case "-":
+                        resultBox.Text = (num1 - num2).ToString();
+                        break;
+                    case "*":
+                        resultBox.Text = (num1 * num2).ToString();
+                        break;
+                    case "/":
+                        resultBox.Text = ((double)num1 / num2).ToString();
+                        break;
+                }
+            }
+            catch(FormatException){
+                MessageBox.Show("Number Invalid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                num1Box.Text = string.Empty;
+                num2Box.Text = string.Empty;
+                num1Box.Focus();
+            }
+            catch(OverflowException){
+                MessageBox.Show("Number Overflow!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                num1Box.Text = string.Empty;
+                num2Box.Text = string.Empty;
+                num1Box.Focus();
+            }
+
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1Box.Text = string.Empty;
+            num2Box.Text = string.Empty;
+            resultBox.Text = string.Empty;
+            num1Box.Focus();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
