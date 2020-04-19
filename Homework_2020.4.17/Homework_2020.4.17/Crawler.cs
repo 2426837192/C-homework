deb4bcde62c7104b8ab7b8930785f88de01f46ab
Yuan Zhong <zhongyuan12345@qq.com>
2020-04-19 19:49:26 +0800

    Updated the homework on April 17, 2020.

---
+++ Homework_2020.4.17/Homework_2020.4.17/Crawler.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework_2020._4._17
+{
+    public delegate void PageDownloadEventHandler(string url);
+    public delegate void PageDownloadErrorEventHandler(Exception e);
+
+    public class Crawler
+    {
+        private Hashtable urls = new Hashtable();
+        public Hashtable URLs { get => urls; set => urls = value; }
+
+        private int count;
+        private string current;
+
+        private string startUrl;
+        public string StartUrl { get => startUrl; set => startUrl = value; }
+
+        public event PageDownloadEventHandler PageDownloaded;
+        public event PageDownloadErrorEventHandler PageDownloadError;
+
+        public Crawler()
+        {
+            count = 0;
+        }
+
+        public Crawler(string startUrl)
+        {
+            count = 0;
+
+            AddStartUrl(startUrl);
+        }
+
+        public void AddStartUrl(string startUrl)
+        {
+            urls.Clear();
+            count = 0;
+
+            startUrl = UrlService.FindAbsolutePath("", startUrl);   // 如果没有https，则加上
+            this.startUrl = startUrl;
+
+            urls.Add(startUrl, false);
+        }
+
+        public void Crawl()
+        {
+            while (true)
+            {
+                current = null;
+
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]){
+                        continue;
+                    }
+
+                    current = url;
+                }
+
+                if (current == null || count > 20)
+                {
+                    Exception endException = new Exception($"爬取结束，共爬取到{count - 1}个网站。");
+                    PageDownloadError(endException);
+
+                    break;
+                }
+
+                if (count > 0) 
+                    PageDownloaded(current);
+
+                string html = "";
+
+                try
+                {
+                    html = DownLoad(current);      // 下载
+                }
+                catch(Exception ex)
+                {
+                    html = "";
+                    PageDownloadError(ex);
+                }
+                finally
+                {
+                    urls[current] = true;
+                    count++;
+
+                    Parse(html);    //解析,并加入新的链接
+                } 
+            }
+        }
+
+        private string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)    //从=后开始，取子串
+                          .Trim('"', '\"', '#', '>');   //去掉首位的空格和特殊符号
+
+                if (strRef.Length == 0)
+                {
+                    continue;
+                }
+                
+                if(!UrlService.IsHtml(strRef))       // 如果不是html文档
+                {
+                    continue;
+                }
+
+                string absolutePath = UrlService.FindAbsolutePath(current, strRef);
+
+                if(!absolutePath.StartsWith(startUrl))    // 如果不是起始网页上的网页
+                {
+                    continue;
+                }
+
+                if (urls[absolutePath] == null)
+                {
+                    urls[absolutePath] = false;
+                }
+            }
+        }
+    }
+}
---
+++ Homework_2020.4.17/WinForm/MainFrom.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Homework_2020._4._17;
+
+namespace WinForm
+{
+    public partial class MainForm : Form
+    {
+        Crawler crawler = new Crawler();
+
+        Thread crawelThread;
+
+        private void AddUrl(string url)
+        {
+            urlListBox.Items.Add(url);
+        }
+
+        private void Crawler_PageDownloaded(string url)
+        {
+            if (this.urlListBox.InvokeRequired)
+            {
+                Action<String> action = this.AddUrl;
+                this.Invoke(action, new object[] { url });
+            }
+            else
+            {
+                AddUrl(url);
+            }
+        }
+
+        private void AddErrorMessage(Exception e)
+        {
+            messageListBox.Items.Add(e.Message);
+        }
+
+        private void Crawler_Error(Exception e)
+        {
+            if (this.messageListBox.InvokeRequired)
+            {
+                Action<Exception> action = this.AddErrorMessage;
+                this.Invoke(action, new object[] { e });
+            }
+            else
+            {
+                AddErrorMessage(e);
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+            crawler.PageDownloadError += Crawler_Error;
+
+            crawelThread = new Thread(crawler.Crawl);
+        }
+
+        private void crawlButton_Click(object sender, EventArgs e)
+        {
+            crawler.AddStartUrl(txtStartUrl.Text);
+
+            urlListBox.Items.Clear();
+            messageListBox.Items.Clear();
+
+            messageListBox.Items.Add("开始爬取。");
+
+            crawelThread = new Thread(crawler.Crawl);
+            crawelThread.Start();
+        }
+    }
+}
